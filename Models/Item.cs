863e818708c47a47dca9429bfbd83eec33d22555
Ahmad Asroruddin <asroruddinahmad09@gmail.com>
2024-06-19 17:09:56 +0700

    create warehouse and item model

---
+++ Models/Item.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace warehouse.Models;
+
+public class Item
+{
+    public int KodeBarang { get; set; }
+    public string NamaBarang { get; set; }
+    public decimal Harga { get; set; }
+    public int Jumlah { get; set; }
+    public DateTime expired { get; set; }
+    public int KodeGudang { get; set; }
+}
---
+++ Models/Warehouse.cs
@@ -0,0 +1,8 @@
+﻿namespace warehouse;
+
+public class Warehouse
+{
+    public int KodeGudang{ get; set; }
+    public string NamaGudang{get; set; }
+}  
+
